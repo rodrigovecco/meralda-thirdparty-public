1e4d538a7ee1804f165af6cea5f4b18b866253b7
Rodrigo Vecco Haddad <rodrigo@novoingenios.com>
2026-07-20 20:21:14 -0500

    Enhance avatar upload process with loading state and spinner

--- cropper/avatar.js
+++ cropper/avatar.js
@@ -60,7 +60,17 @@ CropAvatar.prototype = {
       this.$avatarSave.on('click', $.proxy(this.submitFrm, this));
     },
     submitFrm: function () {
-		//this.$avatarForm.show();
+		// Loading state: disable buttons, show spinner
+		this.$avatarSave.prop('disabled', true).text('Subiendo...');
+		this.$avatarBtns.prop('disabled', true).fadeTo(200, 0.4);
+
+		// Inject spinner if loading div is empty
+		var $ld = this.$loading;
+		if ($ld.length && !$ld.children().length) {
+			$ld.html('<span class="spinner-border spinner-border-sm" role="status"></span>'
+				+ ' <span class="ml-2">Subiendo imagen...</span>');
+		}
+		mw_show_obj($ld);
 		this.$avatarForm.submit();
 		return false;
     },
